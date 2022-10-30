c04afdb9002315660b68ded7b5800038dc7c9d75
S1yad <48476300+S1yad@users.noreply.github.com>
2022-10-30 19:41:03 +0200

    lab2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 /*
@@ -13,9 +13,33 @@ use App\Http\Controllers\UserController;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
 
 Route::resource('users', UserController::class);
+Route::get('/users', [UserController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('users');
+Route::get('/users/create', [UserController::class, 'create'])
+    ->middleware(['auth']);
+Route::get('/users/{id}', [UserController::class, 'show'])
+    ->middleware(['auth']);
+Route::post('/users', [UserController::class, 'store'])
+    ->middleware(['auth']);
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])
+    ->middleware(['auth']);
+Route::put('/users/{user}', [UserController::class, 'update'])
+    ->middleware(['auth']);
+Route::delete('/users/{user}', [UserController::class, 'destroy'])
+    ->middleware(['auth']);
+
+
+require __DIR__.'/auth.php';
